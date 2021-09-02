d2f515064e0b2e45147ed66afb025522bac40e9e
Elisa Nicot <nico0122@algonquinlive.com>
2021-09-02 12:28:56 -0400

    Create index page

---
+++ js/main.js
@@ -0,0 +1,26 @@
+var colour = prompt('A colour (orange, indigo, etc.)');
+var creature = prompt('A creature (dragon, moth, etc.)');
+var adjective = prompt('An adjective (beautiful, super, etc.)');
+var verbed = prompt('A past tense verb (ran, burped, etc.)');
+
+var story = prompt('Which story would you like?\n'+
+                  'a) Snacking\n' +
+                  'b) Escaping\n' +
+                  'c) Winning\n' +
+                  'Enter the letter below:');
+
+var snacking = `After snacking on ${adjective} treats, the ${colour} bellied ${creature} ${verbed}  hours.`;
+var escaping = `Amelia ${verbed} through the ${adjective} ${colour} nebula escaping the space ${creature}.`;
+var winnipeg = `Jackson chose his ${adjective}, ${colour} ${creature} card and ${verbed} it to the table knowing he won.`;
+
+if (story=='a'){
+  document.write(snacking);
+};
+
+if (story == 'b') {
+  document.write(escaping)
+};
+
+if (story == 'c') {
+  document.write(winnipeg)
+};
